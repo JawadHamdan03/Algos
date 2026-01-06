fe2f3c15fcc026b3a4ed890e39b7f4e67684d3cc
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-01-06 21:47:35 +0200

    fix bubble sort

--- CppProject.cpp
+++ CppProject.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 
-void bubleSort(vector<int> &nums)
+// sorting nums[j] and nums[j+1] in i times 
+// O(n^2)
+void bubbleSort(vector<int> &nums)
 {
 	for (int i = 0; i < nums.size(); i++)
 	{
 		for (int j = 0; j < nums.size()-1; j++)
-		{
 			if (nums[j] > nums[j + 1]) swap(nums[j],nums[j+1]);
-		}
 	}
 }
 
@@ -19,7 +19,7 @@ int main()
 {
 	vector<int> vec = { 5,4,3,2,1 };
 	
-	bubleSort(vec);
+	bubbleSort(vec);
 	for (int i : vec)
 		cout << i << " ";
 }
