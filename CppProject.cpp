815280408fda81c08578c7849e3960a5f8afb97e
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-01-10 08:39:10 +0200

    Insertion Sort

--- CppProject.cpp
+++ CppProject.cpp
@@ -1,33 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
 
-// keep finding the idx of the smallest element then swap it with element at i 
-// O(n^2)
-void SelectionSort(vector<int> &nums)
+void insertionSort(vector<int> &arr)
 {
-	for (int i = 0; i < nums.size(); i++)
-	{
-		int idx=i;
-		int smallest = nums[i];
-		for (int j = i+1; j < nums.size() ; j++)
-		{
-			if (nums[j] < nums[i])
-			{
-				idx = j;
-			}
-		}
-		swap(nums[i], nums[idx]);
-	}
+    for (int i = 1; i < arr.size(); i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j>=0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
 }
-
-
 
 int main()
 {
-	vector<int> vec = { 5,4,3,2,1 };
-	
-	SelectionSort(vec);
-	for (int i : vec)
-		cout << i << " ";
+    vector<int> in = {5,2,7,4,6,8};
+    insertionSort(in);
+    for (int i : in) cout << i << " ";
 }
